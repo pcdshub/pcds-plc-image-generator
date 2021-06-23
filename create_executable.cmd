770cb72b00c6e6c67246a4b314b3ef6864208375
K Lauer <klauer@slac.stanford.edu>
2021-06-23 13:21:32 -0700

    Add version in imager filename

--- create_executable.cmd
+++ create_executable.cmd
@@ -1,3 +1,5 @@
 @echo on
 
-pyinstaller create.py --console --onefile -n cx5xxx_pcds_imager --add-data src;src --add-binary CBxx63_WEC7_HPS_v608g_TC31_B4024.10.zip;.
+set VERSION=1
+
+pyinstaller create.py --console --onefile -n cx5xxx_pcds_imager_v%VERSION% --add-data src;src --add-binary CBxx63_WEC7_HPS_v608g_TC31_B4024.10.zip;.

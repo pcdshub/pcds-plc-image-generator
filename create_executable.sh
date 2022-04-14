24707bced59b5fe5f03c9675cb5a32e8050c2d32
Ken Lauer <klauer@slac.stanford.edu>
2022-04-14 16:13:49 -0700

    MNT: bump version number

--- create_executable.sh
+++ create_executable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VERSION=2
+export VERSION=3
 
 pyinstaller console.py --console --onefile -n cx5xxx_pcds_imager_v${VERSION} --add-data src:src
 pyinstaller gui.py --console --onefile -n cx5xxx_pcds_imager_gui_v${VERSION} --add-data src:src
